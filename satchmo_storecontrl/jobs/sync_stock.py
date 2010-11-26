4ab4339ded7b0c887d9bdc8aaef5c31984113597
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2010-11-26 16:48:16 +0100

    Further work on this engine.

--- satchmo_storecontrl/jobs/sync_stock.py
+++ satchmo_storecontrl/jobs/sync_stock.py
@@ -2,15 +2,76 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+from decimal import Decimal
+
 from django_extensions.management.jobs import BaseJob
 
 from satchmo_storecontrl.settings import *
 
+from satchmo_storecontrl.util import Slash2
+
+from product.models import Product
 
 
+def get_product_by_sku(sku):
+    try:
+        product = Product.objects.get(sku=sku)
+
+        logger.debug('Product with SKU %s found.' % sku)
+
+    except Product.DoesNotExist:
+        logger.info('Product with SKU %s not found.' % sku)
+        
+        return None
+
+def update_sku_qty(sku, qty):
+    """ Updates the QTY for a given SKU, if available. """
+    
+    product = get_product_by_sku(sku)
+    
+    if product:
+        product.items_in_stock = Decimal(qty)
+        product.save()
+        
+        return True
+    else:
+        logger.warning('Quantity for SKU %s not updated.' % sku)
+        
+        return False
+    
 
 class Job(BaseJob):
     help = "Synchronise stock with Slash2 SOAP server."
 
     def execute(self):
-        logger.debug('Running stupid job.')
+        logger.debug('Opening connection.')
+        
+        s = Slash2(credentials=SLASH2_CREDENTIALS, endpoint=SLASH2_API_ENDPOINT)
+
+        logger.debug('Fetching max. %d updated products.' % SLASH2_QUERY_LIMIT)
+        
+        products = s.getProductQty({'limit': SLASH2_QUERY_LIMIT})
+        
+        logger.debug('%d products returned.' % len(products))        
+        
+        success_list = []
+        
+        for product in products:
+            success = update_sku_qty(product['sku'], product['qty'])
+            
+            if product['qty'] > 0:
+                logging.debug('Excellent, this product seems to have some information.')
+            
+            if success:
+                success_list.append(product['sku'])
+        
+        logger.info('Updated stock quantity for %d products.' % len(success_list))
+        
+        if success_list:
+            # Remove the SKU's which have been processed, if any have been processed at all
+            success = s.removeSkuFromBuffer(success_list)
+        
+            if success:
+                logger.debug('Removed update SKU\'s from buffer.')
+            else:
+                logging.warning('Error removing SKU\'s from buffer.')
