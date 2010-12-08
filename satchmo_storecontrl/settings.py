e3206b2258b9bbbc6d1101083dd9ced946f7ca7f
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2010-12-08 15:36:41 +0100

    Default value for BTW.

--- satchmo_storecontrl/settings.py
+++ satchmo_storecontrl/settings.py
@@ -4,4 +4,4 @@ SLASH2_API_ENDPOINT = getattr(settings, 'SLASH2_API_ENDPOINT', 'http://scdb.slas
 SLASH2_QUERY_LIMIT = getattr(settings, 'SLASH2_QUERY_LIMIT', 100)
 SLASH2_CREDENTIALS = getattr(settings, 'SLASH2_CREDENTIALS')
 SLASH2_CALCULATE_TAX_AUTO = getattr(settings, 'SLASH2_CALCULATE_TAX_AUTO', False)
-SLASH2_TAX_PERCENT = getattr(settings, 'SLASH2_TAX_PERCENT')
+SLASH2_TAX_PERCENT = getattr(settings, 'SLASH2_TAX_PERCENT', 19)
